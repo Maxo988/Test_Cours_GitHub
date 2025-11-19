73384a72224726da667168a8c83c331bfe09b621
Maxo988 <maelzamitb@gmail.com>
2025-11-19 10:43:35 +0100

    Ajout du projet Bluej

---
+++ Personne.java
@@ -0,0 +1,35 @@
+
+/**
+ * Décrivez votre classe Personne ici.
+ *
+ * @author (votre nom)
+ * @version (un numéro de version ou une date)
+ */
+public class Personne
+{
+    // variables d'instance - remplacez l'exemple qui suit par le vôtre
+    private String nom;
+    private String prenom;
+    private int age;
+
+    /**
+     * Constructeur d'objets de classe Personne
+     */
+    public Personne()
+    {
+        // initialisation des variables d'instance
+        age = 18;
+    }
+
+    /**
+     * Un exemple de méthode - remplacez ce commentaire par le vôtre
+     *
+     * @param  y   le paramètre de la méthode
+     * @return     la somme de x et de y
+     */
+    public String getNom()
+    {
+        // Insérez votre code ici
+        return nom;
+    }
+}
